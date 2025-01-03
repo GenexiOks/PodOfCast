06b376e5716eb66570122811ebe4f28de09265d1
Pavel <Pashaz1994@gmail.com>
2025-01-03 16:55:56 +0300

    work

--- src/component/Testimonial.jsx
+++ src/component/Testimonial.jsx
@@ -1,14 +1,25 @@
-import "./Testimonial.sass";
+/* eslint-disable no-unused-vars */
+import "./Testimonial.scss";
 
-function Testimonial() {
-    <div className="card">
-        <p>"</p>
-        <h1>Lorem ipsum dolor sit amet consectet piscing elit, sed do eiusmod tempor incidi ut labore et dolore magna aliqua. </h1>
-        <div className="card__user-container">
-            <div className="card__user"></div>
-            <div className="card__platform"></div>
-        </div>
-    </div>;
+function Testimonial(props) {
+    return (
+        <>
+            <div className="card">
+                <p>“</p>
+                <h3>Lorem ipsum dolor sit amet consectet piscing elit, sed do eiusmod tempor incidi ut labore et dolore magna aliqua. </h3>
+                <div className="card__user-container">
+                    <div className="card__user">
+                        <img src={props.avatar} alt="avatar1" />
+                        <p>{props.userName},</p>
+                    </div>
+                    <div className="card__platform">
+                        <div className="card__platform-logo">{props.logo}</div>
+                        <div className="card__platform-name">{props.name}</div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default Testimonial;
