b7b78789c49efc61d8e64161dd8b212fa8b81dda
Genexi <Pashaz1994@gmail.com>
2025-01-24 08:59:29 +0300

    25/01/25

--- src/component/NewsCard.jsx
+++ src/component/NewsCard.jsx
@@ -1,6 +1,23 @@
 import "./NewsCard.scss";
 
 function NewsCard() {
-    return <></>;
+    return (
+        <>
+            <div className="news-card">
+                <img src="/blog/img2.png" alt="img" />
+                <div className="news-card__title-container">
+                    <div className="news-card__title">
+                        <p>PODCAST</p>
+                        <h2>Setup your own podcast</h2>
+                    </div>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minimvel iam, quis nostrud exercitation
+                        ullamco laboris...
+                    </p>
+                </div>
+                <div className="news-card__tags-container"></div>
+            </div>
+        </>
+    );
 }
 export default NewsCard;
