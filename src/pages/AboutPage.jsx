63e512f7d5c700389ed4a756028f75be33cbbdc9
Pavel <Pashaz1994@gmail.com>
2025-02-05 17:30:44 +0300

    05/02/25 work

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -89,7 +89,25 @@ const AboutPage = () => {
                         </div>
                     </div>
                 </div>
-                <div className={styles["about-block"]}></div>
+                <div className={styles["about-block"]}>
+                    <img className={styles["about-block__img-background1"]} src="./AboutPage/AboutBlock/vector1.png" alt="img" />
+                    <div className={classNames(styles["about-block__container"], "_container")}>
+                        <div className={styles["title-container"]}>
+                            <h1 className={styles["title-container__title"]}>What our listeners say</h1>
+                            <p className={styles["title-container__text"]}>Their experience throughout every platform</p>
+                        </div>
+                        <img className={styles["about-block__img"]} src="./AboutPage/AboutBlock/img.png" alt="img" />
+                        <img className={styles["about-block__img-background2"]} src="./AboutPage/AboutBlock/vector2.png" alt="img" />
+                        <h2 className={styles["about-block__title"]}>About and History</h2>
+                        <div className={styles["about-block__about"]}>
+                            <div className={styles["about-block__about-text"]}>
+                                <p>Eu non <span>diam</span> phasellus vestibulum lorem. Fringilla est ullamcorper eget nulla facilisi etiam dignissim. Id diam vel quam <span>elementum pulvinar</span>.</p>
+                                <p>Elementum eu facilisis sed odio morbi quis commodo. <br /> Sed odio morbi quis commodo odio aenean sed adipiscing odio diam. uno fablotoson louw uit.</p></div>
+                            <div className={styles["about-block__about-text"]}><p>Lorem ipsum dolor sit amet, <span>consectetur adipiscing</span> elit. Quam pellentesque at bibendum euismod tellus duis cursus dignissim odio. Sit vulputate et integer in. Sit vel, senectus iaculis morbi. <span>Amet</span> interdum imperdiet laoreet <span>morbi</span> tincidunt fermentum, libero. Ante enim eget.</p>
+                                <p>Viverra at porttitor accumsan. <a href="#">Orci non here</a></p></div>
+                        </div>
+                    </div>
+                </div>
             </main>
         </>
     );
