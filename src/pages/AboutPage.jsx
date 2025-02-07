3739ef2315921d298123a9ab6949e85cd17432a3
Pavel <Pashaz1994@gmail.com>
2025-02-07 10:48:29 +0300

    07/02/2025 work

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,8 +1,8 @@
-import React from "react";
-import "./scss/AboutPage.module.scss";
-import { Link } from "react-router-dom";
-import classNames from "classnames";
-import styles from "./scss/AboutPage.module.scss";
+import React from 'react'
+import './scss/AboutPage.module.scss'
+import { Link } from 'react-router-dom'
+import classNames from 'classnames'
+import styles from './scss/AboutPage.module.scss'
 
 const AboutPage = () => {
     return (
@@ -23,7 +23,13 @@ const AboutPage = () => {
                             <li className="nav__element_more more">
                                 <a href="#">More</a>
                                 <div className="more__arrow">
-                                    <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <svg
+                                        width="16"
+                                        height="16"
+                                        viewBox="0 0 16 16"
+                                        fill="none"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                    >
                                         <path
                                             opacity="0.3"
                                             d="M14.6668 7.99992C14.6668 11.6766 11.6761 14.6666 8.0001 14.6666C4.3241 14.6666 1.33344 11.6766 1.33344 7.99992C1.33344 4.32392 4.3241 1.33325 8.0001 1.33325C11.6761 1.33325 14.6668 4.32392 14.6668 7.99992"
@@ -41,76 +47,209 @@ const AboutPage = () => {
                         </ul>
                     </nav>
                     <div className="header__button_container">
-                        <button className="header__button header__button--episodes _button-hover">RECENT EPISODES</button>
+                        <button className="header__button header__button--episodes _button-hover">
+                            RECENT EPISODES
+                        </button>
                         <button className="header__button header__button--subscribe _button-hover">SUBSCRIBE</button>
                     </div>
                 </div>
             </header>
             <main className="page">
-                <div className={classNames(styles["main-block"])}>
-                    <img className={classNames("main-block__img-one", styles["main-block__img-one"])} src="/MainPage/bkgdImg/boom.png" />
-                    <img className={classNames("main-block__img-second", styles["main-block__img-second"])} src="/AboutPage/mainBlock/vector.png" />
-                    <div className={classNames(styles["main-block__container"], "_container")}>
-                        <h1 className={styles["main-block__title"]}>
+                <div className={styles['main-block']}>
+                    <img
+                        className={classNames('main-block__img-one', styles['main-block__img-one'])}
+                        src="/MainPage/bkgdImg/boom.png"
+                    />
+                    <img
+                        className={classNames('main-block__img-second', styles['main-block__img-second'])}
+                        src="/AboutPage/mainBlock/vector.png"
+                    />
+                    <div className={classNames(styles['main-block__container'], '_container')}>
+                        <h1 className={styles['main-block__title']}>
                             About <br /> <span>Pod of Cast</span>
                         </h1>
-                        <p className={styles["main-block__text"]}>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, eiusmod tempor incididunt <br /> ut labore et dolore magna aliqua. Ut enim ad minim veniam–quis.
+                        <p className={styles['main-block__text']}>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, eiusmod tempor incididunt <br /> ut
+                            labore et dolore magna aliqua. Ut enim ad minim veniam–quis.
                         </p>
-                        <div className={styles["main-block__btn-container"]}>
-                            <button className={classNames(styles["main-block__btn-sponsor"], styles["_button"], "_button-hover")}>BECOME SPONSOR</button>
-                            <button className={classNames(styles["main-block__btn-subcribe"], styles["_button"], "_button-hover")}>SUBSCRIBE</button>
+                        <div className={styles['main-block__btn-container']}>
+                            <button
+                                className={classNames(
+                                    styles['main-block__btn-sponsor'],
+                                    styles['_button'],
+                                    '_button-hover'
+                                )}
+                            >
+                                BECOME SPONSOR
+                            </button>
+                            <button
+                                className={classNames(
+                                    styles['main-block__btn-subcribe'],
+                                    styles['_button'],
+                                    '_button-hover'
+                                )}
+                            >
+                                SUBSCRIBE
+                            </button>
                         </div>
-                        <div className={styles["main-block__row"]}>
-                            <div className={styles["main-block__column"]}>
-                                <div className={styles["item-about"]}>
-                                    <h1 className={styles["item-about__total"]}>
+                        <div className={styles['main-block__row']}>
+                            <div className={styles['main-block__column']}>
+                                <div className={styles['item-about']}>
+                                    <h1 className={styles['item-about__total']}>
                                         <span>76</span>K
                                     </h1>
-                                    <p className={styles["item-about__name"]}>Community Members</p>
+                                    <p className={styles['item-about__name']}>Community Members</p>
                                 </div>
                             </div>
-                            <div className={styles["main-block__column"]}>
-                                <div className={styles["item-about"]}>
-                                    <h1 className={styles["item-about__total"]}>
+                            <div className={styles['main-block__column']}>
+                                <div className={styles['item-about']}>
+                                    <h1 className={styles['item-about__total']}>
                                         <span>128</span>K
                                     </h1>
-                                    <p className={styles["item-about__name"]}>Podcast Subscribers</p>
+                                    <p className={styles['item-about__name']}>Podcast Subscribers</p>
                                 </div>
                             </div>
-                            <div className={styles["main-block__column"]}>
-                                <div className={styles["item-about"]}>
-                                    <h1 className={styles["item-about__total"]}>
+                            <div className={styles['main-block__column']}>
+                                <div className={styles['item-about']}>
+                                    <h1 className={styles['item-about__total']}>
                                         <span>59</span>K
                                     </h1>
-                                    <p className={styles["item-about__name"]}>Daily Listeners</p>
+                                    <p className={styles['item-about__name']}>Daily Listeners</p>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className={styles["about-block"]}>
-                    <img className={styles["about-block__img-background1"]} src="./AboutPage/AboutBlock/vector1.png" alt="img" />
-                    <div className={classNames(styles["about-block__container"], "_container")}>
-                        <div className={styles["title-container"]}>
-                            <h1 className={styles["title-container__title"]}>What our listeners say</h1>
-                            <p className={styles["title-container__text"]}>Their experience throughout every platform</p>
+                <div className={styles['about-block']}>
+                    <img
+                        className={styles['about-block__img-background1']}
+                        src="./AboutPage/AboutBlock/vector1.png"
+                        alt="img"
+                    />
+                    <div className={classNames(styles['about-block__container'], '_container')}>
+                        <div className={styles['about-block__backgrounds-img']}>
+                            <img
+                                className={styles['about-block__img-background2']}
+                                src="./AboutPage/AboutBlock/vector2.png"
+                                alt="img"
+                            />
+                            <img
+                                className={styles['about-block__img-background3']}
+                                src="./AboutPage/AboutBlock/vector3.png"
+                                alt="img"
+                            />
                         </div>
-                        <img className={styles["about-block__img"]} src="./AboutPage/AboutBlock/img.png" alt="img" />
-                        <img className={styles["about-block__img-background2"]} src="./AboutPage/AboutBlock/vector2.png" alt="img" />
-                        <h2 className={styles["about-block__title"]}>About and History</h2>
-                        <div className={styles["about-block__about"]}>
-                            <div className={styles["about-block__about-text"]}>
-                                <p>Eu non <span>diam</span> phasellus vestibulum lorem. Fringilla est ullamcorper eget nulla facilisi etiam dignissim. Id diam vel quam <span>elementum pulvinar</span>.</p>
-                                <p>Elementum eu facilisis sed odio morbi quis commodo. <br /> Sed odio morbi quis commodo odio aenean sed adipiscing odio diam. uno fablotoson louw uit.</p></div>
-                            <div className={styles["about-block__about-text"]}><p>Lorem ipsum dolor sit amet, <span>consectetur adipiscing</span> elit. Quam pellentesque at bibendum euismod tellus duis cursus dignissim odio. Sit vulputate et integer in. Sit vel, senectus iaculis morbi. <span>Amet</span> interdum imperdiet laoreet <span>morbi</span> tincidunt fermentum, libero. Ante enim eget.</p>
-                                <p>Viverra at porttitor accumsan. <a href="#">Orci non here</a></p></div>
+                        <div className={styles['title-container']}>
+                            <h1 className={styles['title-container__title']}>What our listeners say</h1>
+                            <p className={styles['title-container__text']}>
+                                Their experience throughout every platform
+                            </p>
+                        </div>
+                        <img className={styles['about-block__img']} src="./AboutPage/AboutBlock/img.png" alt="img" />
+                        <h2 className={styles['about-block__title']}>About and History</h2>
+                        <div className={styles['about-block__about']}>
+                            <div className={styles['about-block__about-text']}>
+                                <p>
+                                    Eu non <span>diam</span> phasellus vestibulum lorem. Fringilla est ullamcorper eget
+                                    nulla facilisi etiam dignissim. Id diam vel quam <span>elementum pulvinar</span>.
+                                </p>
+                                <p>
+                                    Elementum eu facilisis sed odio morbi quis commodo. <br /> Sed odio morbi quis
+                                    commodo odio aenean sed adipiscing odio diam. uno fablotoson louw uit.
+                                </p>
+                            </div>
+                            <div className={styles['about-block__about-text']}>
+                                <p>
+                                    Lorem ipsum dolor sit amet, <span>consectetur adipiscing</span> elit. Quam
+                                    pellentesque at bibendum euismod tellus duis cursus dignissim odio. Sit vulputate et
+                                    integer in. Sit vel, senectus iaculis morbi. <span>Amet</span> interdum imperdiet
+                                    laoreet <span>morbi</span> tincidunt fermentum, libero. Ante enim eget.
+                                </p>
+                                <p>
+                                    Viverra at porttitor accumsan. <a href="#">Orci non here</a>
+                                </p>
+                            </div>
+                        </div>
+                        <h2 className={styles['about-block__bottom-title']}>Founder and Main Host</h2>
+                        <div className={styles['about-block__host-container']}>
+                            <div className={styles['host-card']}>
+                                <div className={styles['host-card__host-photo']}>
+                                    <img src="./AboutPage/AboutBlock/avatar1.png" alt="photo" />
+                                </div>
+                                <img
+                                    className={styles['host-card__background-vector']}
+                                    src="./AboutPage/AboutBlock/hostVector.png"
+                                    alt="vector"
+                                />
+                                <div className={styles['host-card__info']}>
+                                    <div className={styles['host-card__title']}>
+                                        <p className={styles['host-card__host-number']}>Host 1</p>
+                                        <h1 className={styles['host-card__host-name']}>Porter Severus</h1>
+                                    </div>
+                                    <p className={styles['host-card__host-discription']}>
+                                        Lorem ipsum dolor sit amet con sectet piscing elit, sed do eiusmod tempor
+                                        rarylet podofcast.
+                                    </p>
+                                </div>
+                                <div className={styles['host-card__host-sosial-block']}>
+                                    <div className={styles['host-card__host-social']}>
+                                        <p>follow me:</p>
+                                        <div className={styles['host-card__social-icons']}>
+                                            <a href="#">
+                                                <img src="./AboutPage/AboutBlock/tiktok.png" alt="tiktok" />
+                                            </a>
+                                            <a href="#">
+                                                <img src="./AboutPage/AboutBlock/twitter.png" alt="twitter" />
+                                            </a>
+                                            <a href="#">
+                                                <img src="./AboutPage/AboutBlock/instagram.png" alt="instagram" />
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className={styles['host-card']}>
+                                <div className={styles['host-card__host-photo']}>
+                                    <img src="./AboutPage/AboutBlock/avatar2.png" alt="photo" />
+                                </div>
+                                <img
+                                    className={styles['host-card__background-vector']}
+                                    src="./AboutPage/AboutBlock/hostVector2.png"
+                                    alt="vector"
+                                />
+                                <div className={styles['host-card__info']}>
+                                    <div className={styles['host-card__title']}>
+                                        <p className={styles['host-card__host-number']}>Host 2</p>
+                                        <h1 className={styles['host-card__host-name']}>Marques Keiths</h1>
+                                    </div>
+                                    <p className={styles['host-card__host-discription']}>
+                                        Lorem ipsum dolor sit amet con sectet piscing elit, sed do eiusmod tempor
+                                        rarylet podofcast.
+                                    </p>
+                                </div>
+                                <div className={styles['host-card__host-sosial-block']}>
+                                    <div className={styles['host-card__host-social']}>
+                                        <p>follow me:</p>
+                                        <div className={styles['host-card__social-icons']}>
+                                            <a href="#">
+                                                <img src="./AboutPage/AboutBlock/tiktok.png" alt="tiktok" />
+                                            </a>
+                                            <a href="#">
+                                                <img src="./AboutPage/AboutBlock/twitter.png" alt="twitter" />
+                                            </a>
+                                            <a href="#">
+                                                <img src="./AboutPage/AboutBlock/instagram.png" alt="instagram" />
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </main>
         </>
-    );
-};
+    )
+}
 
-export default AboutPage;
+export default AboutPage
